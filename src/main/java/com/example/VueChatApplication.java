ba5d6fb8ed2b9ca16c38e12a4ec4706538d5b60b
tamaitamai <yktmi0217@gmail.com>
2025-04-08 17:50:46 +0900

    初回コミット

---
+++ src/main/java/com/example/VueChatApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueChatApplication.class, args);
+	}
+
+}
